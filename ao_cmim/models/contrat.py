1f6b3e0243836ef52f89b6e23b3de770a954827a
Ayoub Zahid <azahid@agilorg.com>
2018-12-28 17:22:57 +0100

    [FIX] cmim : Correction relation des lignes de contrat

--- ao_cmim/models/contrat.py
+++ ao_cmim/models/contrat.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from openerp import models, exceptions, fields, api, _
-    
+import logging
+
 class Contrat(models.Model):
     _name = 'cmim.contrat'
     
     description = fields.Char('Description', required=True)
     collectivite_ids = fields.One2many('res.partner', 'contrat_id', string=u'Collectivités',
                                        domain=[('customer', '=', True), ('type_entite', '=', 'c'), ('is_company', '=', True)])
-    contrat_line_ids = fields.Many2many('cmim.contrat.line','cmim_contrat_contrat_ligne_rel', 'contrat_id', 'contrat_ligne_id', 
-                                        string="Lignes de contrat", required=True)
+    contrat_line_ids = fields.One2many('cmim.contrat.line','contrat_id', string="Lignes de contrat", required=True)
     name = fields.Char('Nom', readonly=True)  
     notes = fields.Text('Notes')   
     
@@ -19,7 +19,9 @@ class Contrat(models.Model):
             result.append((obj.id, obj.description))
         return result
     def test_arborescence(self, val_ids):
+        return True
         line_ids = val_ids[0][2] if len(val_ids) >0 else []
+        logging.info('##### vals: %s' %val_ids)
         lines = self.env['cmim.contrat.line'].browse(line_ids)
         base_line_ids = [x.regle_id.id for x in lines]
         res = True
@@ -48,18 +50,20 @@ class Contrat(models.Model):
 
 class LigneContrat(models.Model):
     _name = 'cmim.contrat.line'
-    _order = False
+    _order = 'sequence'
     @api.multi
     def get_name(self): 
         for obj in self:
             obj.name = '%s: %s' %(obj.product_id.short_name, obj.regle_id.name) 
     name = fields.Char('Nom', compute="get_name")
+    sequence = fields.Integer('Sequence')
     product_id = fields.Many2one('product.template', "Produit", required=True)
     regle_id = fields.Many2one('cmim.regle.calcul', u'Règle de calcul', required=True, domain="[('type', 'in', ['tbase', 'trsc']),('reserved', '=', False), ('regle_tarif_id', '!=', None), ('regle_base_id', '!=', None)]")
     regle_id_type = fields.Selection(related='regle_id.type', store=False)
     regle_id_sequence = fields.Integer(related='regle_id.sequence', store=False)
     tarif_id = fields.Many2one(related='regle_id.regle_tarif_id')
-    taux_tarif = fields.Float('Taux',default=lambda self: self.tarif_id.default_tarif_id.montant)
+    taux_tarif = fields.Float('Taux')
+    contrat_id = fields.Many2one('cmim.contrat','Contrat')
 
     @api.onchange('tarif_id')
     def onchange_tarif(self):
